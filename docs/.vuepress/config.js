a524b463150cb3d89e8a316ad24bfdfab5adb86d
useryy <484027291@qq.com>
2025-05-26 22:57:16 +0800

    feat:5

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,6 +4,7 @@ import { defineUserConfig } from 'vuepress'
 import { viteBundler } from '@vuepress/bundler-vite'
 
 export default defineUserConfig({
+  base: "/one-price/",
   lang: 'en-US',
 
   title: 'VuePress',
